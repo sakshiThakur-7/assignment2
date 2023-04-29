29f5464c25b3b785befdcfe0c5e3461a62d6ece3
sakshi thakur <122427915+sakshiThakur-7@users.noreply.github.com>
2023-04-29 21:38:53 +0530

    day 3 Assignment submitted

---
+++ day3.py
@@ -0,0 +1,8 @@
+your_name=input("enter your name = ")
+your_age=int(input("enter your current age = "))
+current_year=2023
+
+def age(current_year,your_age):
+    return current_year-your_age
+print("year you will be 100 year old = ",age(current_year,your_age)+100)
+
